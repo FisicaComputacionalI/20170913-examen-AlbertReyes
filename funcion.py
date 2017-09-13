e93a466391e530eaa01e459ced93d0159ed14cbf
AlbertReyes <31415870+AlbertReyes@users.noreply.github.com>
2017-09-13 13:46:49 -0500

    Update funcion.py

--- funcion.py
+++ funcion.py
@@ -1,7 +1,8 @@
 import numpy as np
-import pylab as plt
-x = np.arrange(2012, 2018, 100)
-y = 5*np.cos(2*np.pi*x)+2012
-plt.plot(x,y)
+import pylab as pl
+def f(x):
+    return 5*np.cos(2*np.pi*x)+2012
+x = np.arange(2012, 2018, 100)
+plt.plot(x, f(x))
 plt.savefig('temp.png')
 plt.show()
